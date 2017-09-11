1398883a08a49d7cc7767511b5ce5bb110022ecf
bit-user <bit-user@DESKTOP-C1QR4BV>
2017-09-11 14:49:06 +0900

    mybatis 적용 - user, guestbook

--- src/main/java/com/bigdata2017/mysite/repository/UserDao.java
+++ src/main/java/com/bigdata2017/mysite/repository/UserDao.java
@@ -14,70 +14,14 @@ import org.springframework.stereotype.Repository;
 import com.bigdata2017.mysite.exception.UserDaoException;
 import com.bigdata2017.mysite.vo.UserVo;
 
-import oracle.jdbc.pool.OracleDataSource;
-
 @Repository
 public class UserDao {
-	
-	@Autowired
-	private OracleDataSource oracleDataSource;
 	
 	@Autowired
 	private SqlSession sqlSession;
 	
 	public int update( UserVo userVo ) {
-		
-		int count = 0;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-
-		try {
-			conn = oracleDataSource.getConnection();
-			
-			if( "".equals( userVo.getPassword() ) ) {
-				String sql = 
-					" update member" + 
-					"    set name=?," + 
-					"        gender=?" + 
-					"  where no=?";
-				pstmt = conn.prepareStatement(sql);
-	
-				pstmt.setString(1, userVo.getName() );
-				pstmt.setString(2, userVo.getGender() );
-				pstmt.setLong(3, userVo.getNo() );
-			} else {
-				String sql = 
-						" update member" + 
-						"    set name=?," + 
-						"        gender=?," + 
-						"        password=?" + 
-						"  where no=?";
-				pstmt = conn.prepareStatement(sql);
-		
-				pstmt.setString(1, userVo.getName() );
-				pstmt.setString(2, userVo.getGender() );
-				pstmt.setString(3, userVo.getPassword() );
-				pstmt.setLong(4, userVo.getNo() );				
-			}
-
-			count = pstmt.executeUpdate();
-
-		} catch (SQLException e) {
-			System.out.println("error :" + e);
-		} finally {
-			// 자원 정리
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-
+		int count = sqlSession.update("user.update", userVo );
 		return count;	
 	}
 	
